3bc7ddaaf295dfeeb9aabd8c888d27a8c2d3f44a
drzhn <kleverteo@gmail.com>
2022-11-28 18:25:16 +0400

    Removed branches in tonemapping shader

--- JoyData/shaders/hdrToLdrTransition.hlsl
+++ JoyData/shaders/hdrToLdrTransition.hlsl
@@ -15,18 +15,9 @@ float4 ToneMapping(float4 HDRColor)
 	return HDRColor * LScale;
 }
 
-inline float4 ComputeNonStereoScreenPos(float4 pos)
-{
-	float4 o = pos * 0.5f;
-	o.xy = float2(o.x, o.y * -1) + o.w;
-	o.zw = pos.zw;
-	return o;
-}
-
 struct PSInput
 {
 	float4 position : SV_POSITION;
-	float4 clipPos : TEXCOORD0;
 };
 
 PSInput VSMain(uint id : SV_VertexID)
@@ -34,29 +25,18 @@ PSInput VSMain(uint id : SV_VertexID)
 	float2 uv = float2((id << 1) & 2, id & 2);
 	PSInput input;
 	input.position = float4(uv * float2(2, -2) + float2(-1, 1), 0, 1);
-	input.clipPos = ComputeNonStereoScreenPos(input.position);
 
 	return input;
 }
 
 float4 PSMain(PSInput input) : SV_Target
 {
-	const float2 screenPosition = (input.clipPos.xy / input.clipPos.w);
-
 	float4 color = HdrTexture.Load(float3(input.position.xy, 0));
 
-	//color += BloomScale * BloomTexture.Sample(LinearSampler, screenPosition);
+	const float gamma = 2.2;
+	color.rgb = lerp(color.rgb, pow(color.rgb, float3(1, 1, 1) * (1.0 / gamma)), Constants.UseGammaCorrection);
 
-	if (Constants.UseGammaCorrection)
-	{
-		float gamma = 2.2;
-		color.rgb = pow(color.rgb, float3(1, 1, 1) * (1.0 / gamma));
-	}
-
-	if (Constants.UseTonemapping)
-	{
-		color = ToneMapping(color);
-	}
+	color = lerp(color, ToneMapping(color), Constants.UseTonemapping);
 
 	return color;
 }
